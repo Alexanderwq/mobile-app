36ef1de32a74ed2d24e12247010f2cd25c420d86
Alexander Titov <at@truelabel.io>
2024-09-16 12:31:50 +0300

    Добавлена кнопка назад

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -1,8 +1,14 @@
-import {Stack} from "expo-router";
+import {router, Stack} from "expo-router";
+import {List} from "react-native-paper";
+import {TouchableOpacity} from "react-native";
 
 export default function PageLayout() {
     return (
-        <Stack>
+        <Stack screenOptions={{ headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <List.Icon color="rgba(1,165,221,1.00)" icon="arrow-left"/>
+            </TouchableOpacity>
+          )}}>
           <Stack.Screen name="schedule" options={{ title: 'Расписание' }} />
           <Stack.Screen name="team" options={{ title: 'Наша команда' }} />
         </Stack>
