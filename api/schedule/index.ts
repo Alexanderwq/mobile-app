3411a97c4e384f5dac8f6fda799cbb7570ab8df0
alexander <lokistapway@yandex.ru>
2024-10-30 22:22:49 +0300

    Поправлен формат

--- api/schedule/index.ts
+++ api/schedule/index.ts
@@ -2,6 +2,6 @@ import api from "@/api/axiosInstance";
 import ScheduleListItemInterface from "@/api/types/ScheduleListItemInterface";
 
 export async function getScheduleList(date: Date): Promise<ScheduleListItemInterface[]> {
-  const formattedDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+  const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
   return (await api.get(`/api/schedule?date=${formattedDate}`)).data
 }
