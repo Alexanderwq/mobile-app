021b8c159ae6605f61ebfac17d6f86d06e11425c
Titov Alexander <alex.t@truelabel.io>
2024-12-23 22:00:19 +0300

    Убран прелоадер

--- components/ProfilePaymentBlock.tsx
+++ components/ProfilePaymentBlock.tsx
@@ -11,7 +11,7 @@ export default function MainMenu() {
   })
 
   if (isPending) return (
-    <ActivityIndicator animating={true} color="#01a5dd" size="large"/>
+      ''
   )
 
   if (error) return (
