db7d1325e0cbc7e104f2cc33990df086f6a95a4e
Titov Alexander <alex.t@truelabel.io>
2025-01-28 15:11:09 +0300

    Исправлена ссылка на карту

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,12 @@
-import {View} from 'react-native';
+import {Platform, View} from 'react-native';
 import MainMenu from "@/components/MainMenu";
 
 export default function HomeScreen() {
+
+  const mapLink = Platform.select({
+    ios: 'maps:0,0?q=57,58336,39,90045',
+    android: "https://yandex.ru/maps/16/yaroslavl/house/dyadkovskaya_ulitsa_7/Z0AYfwdgTUcCQFttfXl5cnhiYg==/?ll=39.900414%2C57.583421&z=17"
+  })
   const links = [
     {
       title: 'Расписание тренировок',
@@ -16,7 +21,7 @@ export default function HomeScreen() {
     {
       title: 'Мы на карте',
       icon: 'map',
-      href: "https://yandex.ru/maps/16/yaroslavl/house/dyadkovskaya_ulitsa_7/Z0AYfwdgTUcCQFttfXl5cnhiYg==/?ll=39.900414%2C57.583421&z=17"
+      href: mapLink,
     },
     {
       title: 'Наша команда',
